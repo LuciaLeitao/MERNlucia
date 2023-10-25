f4f97cb8fae2b5911b32c6d86f5dea4729e73a3a
lucia-leitao <lucialeitao85@gmail.com>
2023-10-25 15:11:01 +0100

    complete show user listings functionality - bug solved

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -237,7 +237,7 @@ export default function Profile() {
         </span>
       </div>
 
-      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-red-700 mt-5">{error ? error : " "}</p>
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated successfully!" : ""}
       </p>
@@ -245,15 +245,13 @@ export default function Profile() {
         Show Listings
       </button>
       <p className="text-red-700 mt-5">
-        {showListingsError ? "Error showing listings" : ""}
+        {showListingsError ? "Error showing listings" : " "}
       </p>
 
-      {userListings && userListings.length > 0 && (
-        <div className="flex flex-col gap-4">
-          <h1 className="text-center mt-7 text-2xl font-semibold">
-            Your Listings
-          </h1>
-          {userListings.map((listing) => (
+      {userListings && userListings.length > 0 && 
+      <div className="flex flex-col gap-4">
+      <h1 className="text-center mt-7 text-2xl font-semibold uppercase">Your Listing</h1>
+      {  userListings.map((listing) => (
             <div
               key={listing._id}
               className="border rounded-lg p-3 flex justify-between items-center gap-4"
@@ -265,11 +263,10 @@ export default function Profile() {
                   className="h-16 w-16 object-contain"
                 />
               </Link>
-              <Link
-                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
-                to={`/listing/${listing._id}`}
+              <Link to={`/listing/${listing._id}`}
+              className="text-slate-700 font-semibold  hover:underline truncate flex-1"
               >
-                <p>{listing.name}</p>
+                <p >{listing.name}</p>
               </Link>
 
               <div className="flex flex-col item-center">
@@ -285,8 +282,9 @@ export default function Profile() {
               </div>
             </div>
           ))}
+        </div>}
         </div>
       )}
-    </div>
-  );
-}
+    
+
+
